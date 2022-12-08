ebd605aff09d52019c0b4534376838c99a53dda5
ikusner <ikusner@efuse.io>
2022-12-08 08:54:12 -0500

    part 2

--- 08/puzzle.ts
+++ 08/puzzle.ts
@@ -40,7 +40,88 @@ export const part1 = (input: string) => {
 };
 
 export const part2 = (input: string) => {
-  return input.split("\n").length;
+  type Max = {
+    position: number;
+    value: number;
+  };
+
+  const rowMax: Max[] = [];
+  const colMax: Max[] = [];
+
+  let bestPositionValue = 0;
+
+  const split = input.split("\n").map((line) => line.split("").map(Number));
+
+  for (let i = 0; i < split.length; i++) {
+    for (let j = 0; j < split[i].length; j++) {
+      if (i > 0 && i < split.length - 1 && j > 0 && j < split[i].length - 1) {
+        const tree = split[i][j];
+
+        const rightBlockingTreeValue = split[i].slice(j + 1, split[i].length)
+          .find(
+            (value) => value >= tree,
+          ) ?? -1;
+
+        const rightBlockingTreePosition = split[i].slice(j + 1, split[i].length)
+          .indexOf(rightBlockingTreeValue);
+
+        const rightBlockingTree: Max = {
+          value: rightBlockingTreeValue,
+          position: rightBlockingTreePosition === -1
+            ? -1
+            : rightBlockingTreePosition + j + 1,
+        };
+
+        const bottomBlockingTreeValue = split.map((row) => row[j]).slice(
+          i + 1,
+          split.length,
+        ).find((value) => value >= tree) ?? -1;
+
+        const bottomBlockingTreePosition = split.map((row) => row[j]).slice(
+          i + 1,
+          split.length,
+        )
+          .indexOf(
+            bottomBlockingTreeValue,
+          );
+
+        const bottomBlockingTree: Max = {
+          value: bottomBlockingTreeValue,
+          position: bottomBlockingTreePosition == -1
+            ? -1
+            : bottomBlockingTreePosition + i + 1,
+        };
+
+        const rightScore = rightBlockingTree.value !== -1
+          ? rightBlockingTree.position - j
+          : split[i].length - 1 - j;
+
+        const bottomScore = bottomBlockingTree.value !== -1
+          ? bottomBlockingTree.position - i
+          : split.length - 1 - i;
+
+        const topScore = colMax[j].value >= tree ? i - colMax[j].position : i;
+
+        const leftScore = rowMax[i].value >= tree ? j - rowMax[i].position : j;
+
+        const totalScore = rightScore * bottomScore * topScore * leftScore;
+
+        if (totalScore > bestPositionValue) {
+          bestPositionValue = totalScore;
+        }
+      }
+
+      if (split[i][j] >= rowMax[i]?.value || rowMax[i]?.value === undefined) {
+        rowMax[i] = { position: j, value: split[i][j] };
+      }
+
+      if (split[i][j] >= colMax[j]?.value || colMax[j]?.value === undefined) {
+        colMax[j] = { position: i, value: split[i][j] };
+      }
+    }
+  }
+
+  return bestPositionValue;
 };
 
 export const main = () => {
@@ -49,7 +130,7 @@ export const main = () => {
   const input = Deno.readTextFileSync(file);
 
   console.log("part1", part1(input));
-  // console.log("part2", part2(input));
+  console.log("part2", part2(input));
 };
 
 import.meta.main && main();
